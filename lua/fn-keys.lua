696ab9c5259aea8219fbdfa470b8ed4b83ff1f26
Siliang Cao <siliang.cao@gmail.com>
2022-03-13 13:53:40 +0800

    Only keep one "close" shortcut

--- lua/fn-keys.lua
+++ lua/fn-keys.lua
@@ -7,9 +7,9 @@ vim.api.nvim_set_keymap('i', '<F3>', "<C-c><cmd>lua require('fzf-lua').files()<C
 vim.api.nvim_set_keymap('n', '<F3>', "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('v', '<F3>', "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true })
 -- Close
-vim.api.nvim_set_keymap('i', '<F4>', "<cmd>close<CR>", { noremap = true, silent = false })
-vim.api.nvim_set_keymap('n', '<F4>', "<cmd>close<CR>", { noremap = true, silent = false })
-vim.api.nvim_set_keymap('v', '<F4>', "<cmd>close<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap('i', '<F4>', "<cmd>q<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '<F4>', "<cmd>q<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap('v', '<F4>', "<cmd>q<CR>", { noremap = true, silent = false })
 -- Search file
 vim.api.nvim_set_keymap('i', '<F5>', "<C-c><cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<F5>', "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true })
@@ -22,10 +22,6 @@ vim.api.nvim_set_keymap('v', '<F6>', "<cmd>wa<CR>", { noremap = true, silent = f
 vim.api.nvim_set_keymap('i', '<F7>', '<cmd>lua require("nvim-tree").toggle(true, true)<CR>', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<F7>', ':NvimTreeFindFileToggle<CR>', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('v', '<F7>', '<C-c>:NvimTreeFindFileToggle<CR>', { noremap = false, silent = true })
--- Quit
-vim.api.nvim_set_keymap('i', '<F8>', '<cmd>qa<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F8>', '<cmd>qa<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<F8>', '<cmd>qa<CR>', { noremap = true, silent = true })
 
 -- Command Key Copy/Paste
 -- use command + c
